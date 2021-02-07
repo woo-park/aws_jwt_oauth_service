fa44c38169662c9c7bbbb519640703caeb79895a
Woo <leo9634@naver.com>
2021-02-06 23:08:33 -0500

    EnableJpaAuditing anotation off pg 221

--- src/main/java/com/awsjwtservice/AuthServerApplication.java
+++ src/main/java/com/awsjwtservice/AuthServerApplication.java
@@ -7,7 +7,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 
 //@EnableResourceServer
-@EnableJpaAuditing  // JPA Auditing 활성화
+//@EnableJpaAuditing  // JPA Auditing 활성화
 @SpringBootApplication
 public class AuthServerApplication {
     public static void main(String[] args) {
