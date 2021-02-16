a41f4c3188048ace3ba423309814d1fc8431afd1
Woo <leo9634@naver.com>
2021-02-16 05:42:45 -0500

    deploy.sh jar 이 아닌 java

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -9,7 +9,7 @@ cp $REPOSITORY/zip/*.jar $REPOSITORY/
 
 echo "> 현재 구동중인 애플리케이션 pid 확인"
 
-CURRENT_PID=$(pgrep -fl aws_jwt_oauth_service | grep jar | awk '{print $1}')
+CURRENT_PID=$(pgrep -fl aws_jwt_oauth_service | grep java | awk '{print $1}')
 TEST_CURRENT_PID=$(pgrep -fl aws_jwt_oauth_service)
 
 echo "현재 구동중인 어플리케이션 pid: $CURRENT_PID"
