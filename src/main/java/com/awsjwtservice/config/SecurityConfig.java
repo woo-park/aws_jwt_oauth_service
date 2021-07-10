0804acd216514ff480cc23959bc1a79411827f63
woo-park <wp503@nyu.edu>
2021-07-10 22:49:29 +0900

    update -> login page

--- src/main/java/com/awsjwtservice/config/SecurityConfig.java
+++ src/main/java/com/awsjwtservice/config/SecurityConfig.java
@@ -133,6 +133,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         http.authorizeRequests()
                 .antMatchers("/css/base.css").permitAll()
                 .antMatchers("/css/style.css").permitAll()
+                .antMatchers("/css/login.css").permitAll()
 
                 .antMatchers("/denied").permitAll()
                 .antMatchers("/oauth_login","/oauth_login/**","/login_proc", "/oauth2/authorize-client","/oauth2/authorize-client/**", "/loginSuccess", "/loginFailure", "/loginSuccess/**", "/loginFailure/**", "/", "/h2-console", "/h2-console/**").permitAll()
