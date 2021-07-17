a83ed4ae4213cf6788a90a5508ba644cade85657
Woo <leo9634@naver.com>
2021-07-17 05:08:20 -0400

    rollback -> testing

--- src/main/java/com/awsjwtservice/controller/OrderController.java
+++ src/main/java/com/awsjwtservice/controller/OrderController.java
@@ -1,88 +1,88 @@
-package com.awsjwtservice.controller;
-
-
-import com.awsjwtservice.domain.Account;
-import com.awsjwtservice.domain.OrderItem;
-import com.awsjwtservice.domain.OrderSearch;
-import com.awsjwtservice.domain.Orders;
-import com.awsjwtservice.domain.item.Item;
-import com.awsjwtservice.dto.OrdersDto;
-import com.awsjwtservice.service.AccountService;
-import com.awsjwtservice.service.ItemService;
-import com.awsjwtservice.service.OrderService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.*;
-
-import java.util.ArrayList;
-import java.util.List;
-import java.util.stream.Collectors;
-
-@Controller
-public class OrderController {
-
-    @Autowired
-    OrderService orderService;
-
-    @Autowired
-    AccountService userService;
-
-    @Autowired
-    ItemService itemService;
-
-
-    /* make order */
-    @RequestMapping(value = "/order", method = RequestMethod.GET)
-    public String createForm(Model model) {
-
-        List<Account> users = userService.getUsers();
-//        List<Item> items = itemService.findItems();
-
-//        model.addAttribute("users", users);
-//        model.addAttribute("items", items);
-
-
-        return "order/orderForm";
-    }
-
-//    @RequestMapping(value = "/order", method = RequestMethod.POST)
-//    public String order(@RequestParam("userId") long memberId, @RequestParam("itemId") long itemId, @RequestParam("count") int count) {
+//package com.awsjwtservice.controller;
 //
-//        orderService.order(memberId, itemId, count);
+//
+//import com.awsjwtservice.domain.Account;
+//import com.awsjwtservice.domain.OrderItem;
+//import com.awsjwtservice.domain.OrderSearch;
+//import com.awsjwtservice.domain.Orders;
+//import com.awsjwtservice.domain.item.Item;
+//import com.awsjwtservice.dto.OrdersDto;
+//import com.awsjwtservice.service.AccountService;
+//import com.awsjwtservice.service.ItemService;
+//import com.awsjwtservice.service.OrderService;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.stereotype.Controller;
+//import org.springframework.ui.Model;
+//import org.springframework.web.bind.annotation.*;
+//
+//import java.util.ArrayList;
+//import java.util.List;
+//import java.util.stream.Collectors;
+//
+//@Controller
+//public class OrderController {
+//
+//    @Autowired
+//    OrderService orderService;
+//
+//    @Autowired
+//    AccountService userService;
+//
+//    @Autowired
+//    ItemService itemService;
+//
+//
+//    /* make order */
+//    @RequestMapping(value = "/order", method = RequestMethod.GET)
+//    public String createForm(Model model) {
+//
+//        List<Account> users = userService.getUsers();
+////        List<Item> items = itemService.findItems();
+//
+////        model.addAttribute("users", users);
+////        model.addAttribute("items", items);
+//
+//
+//        return "order/orderForm";
+//    }
+//
+////    @RequestMapping(value = "/order", method = RequestMethod.POST)
+////    public String order(@RequestParam("userId") long memberId, @RequestParam("itemId") long itemId, @RequestParam("count") int count) {
+////
+////        orderService.order(memberId, itemId, count);
+////        return "redirect:/orders";
+////    }
+//
+////    @RequestMapping(value = "/orders", method = RequestMethod.GET)
+////    public String orderList(@ModelAttribute("orderSearch") OrderSearch orderSearch, Model model) {
+////
+////        List<Orders> orders = orderService.findOrders(orderSearch);
+//////        List<OrdersDto> ordersDto = new ArrayList<OrdersDto>();       // used to be like this, but i changed to using dtos
+////
+////        List<OrdersDto> ordersDto = orders.stream().map(e ->
+////                OrdersDto.builder()
+////                        .id(e.getId())
+////                        .delivery(e.getDelivery())
+////                        .orderDate(e.getOrderDate())
+////                        .status(e.getStatus())
+////                        .username(e.getAccount().getUsername())
+////                        .orderItems(e.getOrderItems())
+////                        .orderBoolean(Boolean.valueOf(e.getStatus().toString() == "ORDER" ? true : false))
+////                        .build()).collect(Collectors.toList());
+////
+////
+////        model.addAttribute("orders", ordersDto);
+////
+////        return "order/orderList";
+////    }
+//
+//
+//    @RequestMapping(value = "/orders/{orderId}/cancel")
+//    public String processCancelBuy(@PathVariable("orderId") Long orderId) {
+//
+//        orderService.cancelOrder(orderId);
+//
 //        return "redirect:/orders";
 //    }
-
-//    @RequestMapping(value = "/orders", method = RequestMethod.GET)
-//    public String orderList(@ModelAttribute("orderSearch") OrderSearch orderSearch, Model model) {
 //
-//        List<Orders> orders = orderService.findOrders(orderSearch);
-////        List<OrdersDto> ordersDto = new ArrayList<OrdersDto>();       // used to be like this, but i changed to using dtos
-//
-//        List<OrdersDto> ordersDto = orders.stream().map(e ->
-//                OrdersDto.builder()
-//                        .id(e.getId())
-//                        .delivery(e.getDelivery())
-//                        .orderDate(e.getOrderDate())
-//                        .status(e.getStatus())
-//                        .username(e.getAccount().getUsername())
-//                        .orderItems(e.getOrderItems())
-//                        .orderBoolean(Boolean.valueOf(e.getStatus().toString() == "ORDER" ? true : false))
-//                        .build()).collect(Collectors.toList());
-//
-//
-//        model.addAttribute("orders", ordersDto);
-//
-//        return "order/orderList";
-//    }
-
-
-    @RequestMapping(value = "/orders/{orderId}/cancel")
-    public String processCancelBuy(@PathVariable("orderId") Long orderId) {
-
-        orderService.cancelOrder(orderId);
-
-        return "redirect:/orders";
-    }
-
-}
+//}
--- src/main/java/com/awsjwtservice/domain/OrderItem.java
+++ src/main/java/com/awsjwtservice/domain/OrderItem.java
@@ -1,100 +1,100 @@
-package com.awsjwtservice.domain;
-
-
-import com.awsjwtservice.domain.item.Item;
-
-import javax.persistence.*;
-
-@Entity
-@Table(name = "order_item")
-public class OrderItem {
-
-    @Id @GeneratedValue
-    @Column(name = "order_item_id")
-    private Long id;
-
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "item_id")
-    private Item item;      //주문 상품
-
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "order_id")
-    private Orders order;    //주문
-
-    private int orderPrice; //주문 가격
-    private int count;      //주문 수량
-
-    //==생성 메서드==//
-    public static OrderItem createOrderItem(Item item, int orderPrice, int count) {
-
-        OrderItem orderItem = new OrderItem();
-        orderItem.setItem(item);
-        orderItem.setOrderPrice(orderPrice);
-        orderItem.setCount(count);
-
-        item.removeStock(count);
-        return orderItem;
-    }
-
-    //==비즈니스 로직==//
-    /** 주문 취소 */
-    public void cancel() {
-        getItem().addStock(count);
-    }
-
-    //==조회 로직==//
-    /** 주문상품 전체 가격 조회 */
-    public int getTotalPrice() {
-        return getOrderPrice() * getCount();
-    }
-
-    //==Getter, Setter==//
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public Item getItem() {
-        return item;
-    }
-
-    public void setItem(Item item) {
-        this.item = item;
-    }
-
-    public Orders getOrder() {
-        return order;
-    }
-
-    public void setOrder(Orders order) {
-        this.order = order;
-    }
-
-    public int getOrderPrice() {
-        return orderPrice;
-    }
-
-    public void setOrderPrice(int buyPrice) {
-        this.orderPrice = buyPrice;
-    }
-
-    public int getCount() {
-        return count;
-    }
-
-    public void setCount(int count) {
-        this.count = count;
-    }
-
-    @Override
-    public String toString() {
-        return "OrderItem{" +
-                "id=" + id +
-                ", buyPrice=" + orderPrice +
-                ", count=" + count +
-                '}';
-    }
-}
+//package com.awsjwtservice.domain;
+//
+//
+//import com.awsjwtservice.domain.item.Item;
+//
+//import javax.persistence.*;
+//
+//@Entity
+//@Table(name = "order_item")
+//public class OrderItem {
+//
+//    @Id @GeneratedValue
+//    @Column(name = "order_item_id")
+//    private Long id;
+//
+//    @ManyToOne(fetch = FetchType.LAZY)
+//    @JoinColumn(name = "item_id")
+//    private Item item;      //주문 상품
+//
+//    @ManyToOne(fetch = FetchType.LAZY)
+//    @JoinColumn(name = "order_id")
+//    private Orders order;    //주문
+//
+//    private int orderPrice; //주문 가격
+//    private int count;      //주문 수량
+//
+//    //==생성 메서드==//
+//    public static OrderItem createOrderItem(Item item, int orderPrice, int count) {
+//
+//        OrderItem orderItem = new OrderItem();
+//        orderItem.setItem(item);
+//        orderItem.setOrderPrice(orderPrice);
+//        orderItem.setCount(count);
+//
+//        item.removeStock(count);
+//        return orderItem;
+//    }
+//
+//    //==비즈니스 로직==//
+//    /** 주문 취소 */
+//    public void cancel() {
+//        getItem().addStock(count);
+//    }
+//
+//    //==조회 로직==//
+//    /** 주문상품 전체 가격 조회 */
+//    public int getTotalPrice() {
+//        return getOrderPrice() * getCount();
+//    }
+//
+//    //==Getter, Setter==//
+//    public Long getId() {
+//        return id;
+//    }
+//
+//    public void setId(Long id) {
+//        this.id = id;
+//    }
+//
+//    public Item getItem() {
+//        return item;
+//    }
+//
+//    public void setItem(Item item) {
+//        this.item = item;
+//    }
+//
+//    public Orders getOrder() {
+//        return order;
+//    }
+//
+//    public void setOrder(Orders order) {
+//        this.order = order;
+//    }
+//
+//    public int getOrderPrice() {
+//        return orderPrice;
+//    }
+//
+//    public void setOrderPrice(int buyPrice) {
+//        this.orderPrice = buyPrice;
+//    }
+//
+//    public int getCount() {
+//        return count;
+//    }
+//
+//    public void setCount(int count) {
+//        this.count = count;
+//    }
+//
+//    @Override
+//    public String toString() {
+//        return "OrderItem{" +
+//                "id=" + id +
+//                ", buyPrice=" + orderPrice +
+//                ", count=" + count +
+//                '}';
+//    }
+//}
