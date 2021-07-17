42507faa37dc9080ef284485208528530df0b582
Woo <leo9634@naver.com>
2021-07-17 12:12:40 -0400

    test -> repository em persistence

--- src/main/java/com/awsjwtservice/controller/ItemController.java
+++ src/main/java/com/awsjwtservice/controller/ItemController.java
@@ -1,69 +1,69 @@
-//package com.awsjwtservice.controller;
-//
-//import com.awsjwtservice.domain.item.Book;
-//import com.awsjwtservice.domain.item.Item;
-//import com.awsjwtservice.service.ItemService;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.stereotype.Controller;
-//import org.springframework.ui.Model;
-//import org.springframework.web.bind.annotation.ModelAttribute;
-//import org.springframework.web.bind.annotation.PathVariable;
-//import org.springframework.web.bind.annotation.RequestMapping;
-//import org.springframework.web.bind.annotation.RequestMethod;
-//
-//import java.util.List;
-//
-//@Controller
-//public class ItemController {
-//
-//    @Autowired
-//    ItemService itemService;
-//
-//    @RequestMapping(value = "/items/new", method = RequestMethod.GET)
-//    public String createForm() {
-//        return "items/createItemForm";
-//    }
-//
-//    @RequestMapping(value = "/items/new", method = RequestMethod.POST)
-//    public String create(Book item) {
-//
-////        itemService.saveItem(item);
-//        return "redirect:/items";
-//    }
-//
-//    /**
-//     * 상품 수정 폼
-//     */
-//    @RequestMapping(value = "/items/{itemId}/edit", method = RequestMethod.GET)
-//    public String updateItemForm(@PathVariable("itemId") long itemId, Model model) {
-//
-////        Item item = itemService.findOne(itemId);
-////        model.addAttribute("item", item);
-//        return "items/updateItemForm";
-//    }
-//
-//    /**
-//     * 상품 수정
-//     */
-//    @RequestMapping(value = "/items/{itemId}/edit", method = RequestMethod.POST)
-//    public String updateItem(@ModelAttribute("item") Book item) {
-//
-////        itemService.saveItem(item);
-//        return "redirect:/items";
-//    }
-//
-//
-//
-//    /**
-//     * 상품 목록
-//     */
-//    @RequestMapping(value = "/items", method = RequestMethod.GET)
-//    public String list(Model model) {
-//
-////        List<Item> items = itemService.findItems();
-//
-////        model.addAttribute("items", items);
-//        return "items/itemList";
-//    }
-//
-//}
+package com.awsjwtservice.controller;
+
+import com.awsjwtservice.domain.item.Book;
+import com.awsjwtservice.domain.item.Item;
+import com.awsjwtservice.service.ItemService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import java.util.List;
+
+@Controller
+public class ItemController {
+
+    @Autowired
+    ItemService itemService;
+
+    @RequestMapping(value = "/items/new", method = RequestMethod.GET)
+    public String createForm() {
+        return "items/createItemForm";
+    }
+
+    @RequestMapping(value = "/items/new", method = RequestMethod.POST)
+    public String create(Book item) {
+
+//        itemService.saveItem(item);
+        return "redirect:/items";
+    }
+
+    /**
+     * 상품 수정 폼
+     */
+    @RequestMapping(value = "/items/{itemId}/edit", method = RequestMethod.GET)
+    public String updateItemForm(@PathVariable("itemId") long itemId, Model model) {
+
+//        Item item = itemService.findOne(itemId);
+//        model.addAttribute("item", item);
+        return "items/updateItemForm";
+    }
+
+    /**
+     * 상품 수정
+     */
+    @RequestMapping(value = "/items/{itemId}/edit", method = RequestMethod.POST)
+    public String updateItem(@ModelAttribute("item") Book item) {
+
+//        itemService.saveItem(item);
+        return "redirect:/items";
+    }
+
+
+
+    /**
+     * 상품 목록
+     */
+    @RequestMapping(value = "/items", method = RequestMethod.GET)
+    public String list(Model model) {
+
+//        List<Item> items = itemService.findItems();
+
+//        model.addAttribute("items", items);
+        return "items/itemList";
+    }
+
+}
